6441e230189f0e304af7dc0ef3d77696144dbd10
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-03-09 16:00:34 +0100

    count up and down

---
+++ dart/s2_big_o_notation/count_up_and_down.dart
@@ -0,0 +1,16 @@
+void countUpAndDown(final int n){
+  print("Going up!");
+  for (int i = 0; i < n; i++) {
+    print(i);
+  }
+  print("At the top!\nGoing down...");
+
+  for (int j = n -1; j >= 0; j--) {
+    print(j);
+  }
+  print("Back down. Bye!");
+}
+
+void main() {
+  countUpAndDown(12);
+}
