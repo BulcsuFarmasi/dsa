cef5c81920dbe84a264e67fe52fd9ddaaf7b8e2c
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-08-07 17:22:27 +0200

    arrray / list

---
+++ js/s3-array-object-performance/array.js
@@ -0,0 +1,4 @@
+const names = ["Micheal", "Melissa", "Andrea"];
+//             "Micheal"  "Melissa"  "Andrea"    "Raj"
+//                 0          1         2          3
+const values = [true, {}, [], 2, "awesome"];
