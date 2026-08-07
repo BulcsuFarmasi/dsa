c5e4db3acefe45fb400dffcb15d4204b73699801
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-08-07 19:17:05 +0200

    basic solution

--- dart/s4_problem_solving_approach/char_count.dart
+++ dart/s4_problem_solving_approach/char_count.dart
@@ -1,8 +1,24 @@
 Map<String, int> charCount(final String str) {
     // make object
+    final Map<String, int> result = {};
     // loop over string, for each character
-        // if the char is a number / letter AND a key in object, add one to count
-        // if the char is a number / letter AND  not in object, add it and set value to 1
+    for (int i = 0; i < str.length; i++) {
+      final String char = str[i];
+      // if the char is a number / letter AND a key in object, add one to count
+      if (result[char] != null) {
+        result[char] = result[char]! + 1;
+      // if the char is a number / letter AND  not in object, add it and set value to 1
+      } else {
+        result[char] = 1;
+      }
+    }
+
+
         // if character is something else (space, period, etc. ) don't do anything
     // return object at end
+    return result;
+}
+
+void main() {
+  print(charCount("hello"));
 }
--- js/s4-problem-solving-approach/char-count.js
+++ js/s4-problem-solving-approach/char-count.js
@@ -1,8 +1,23 @@
 function charCount(str) {
     // make object
+    const result = {};
     // loop over string, for each character
+    for (let i = 0; i < str.length; i++) {
+
+       let char = str[i];
         // if the char is a number / letter AND a key in object, add one to count
+        if (result[char] > 0) {
+            result[char]++;
+        
         // if the char is a number / letter AND  not in object, add it and set value to 1
+        } else {
+            result[char] = 1;
+        }
+    }
+
         // if character is something else (space, period, etc. ) don't do anything
     // return object at end
+    return result;
 }
+
+console.log(charCount("hello"));
