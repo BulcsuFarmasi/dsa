4e6dd0ba5e5b4fe22f2db7197aaa47fb04fe0587
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-08-07 18:51:18 +0200

    most basic breakdown

---
+++ dart/s4_problem_solving_approach/char_count.dart
@@ -0,0 +1,4 @@
+Map<String, int> charCount(final String str) {
+    // do something
+    // return an object with key that are lowercase alphanumberic characters in the string; value should be be the counts for those characters
+}
---
+++ js/s4-problem-solving-approach/char-count.js
@@ -0,0 +1,4 @@
+function charCount(str) {
+    // do something
+    // return an object with key that are lowercase alphanumberic characters in the string; value should be be the counts for those characters
+}
