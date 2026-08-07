6095c9f2dd59bb4c5a120010ccc74e35055d42db
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-08-07 19:01:33 +0200

    expand loops

--- dart/s4_problem_solving_approach/char_count.dart
+++ dart/s4_problem_solving_approach/char_count.dart
@@ -1,5 +1,8 @@
 Map<String, int> charCount(final String str) {
     // make object
-    // loop over string
+    // loop over string, for each character
+        // if the char is a number / letter AND a key in object, add one to count
+        // if the char is a number / letter AND  not in object, add it and set value to 1
+        // if character is something else (space, period, etc. ) don't do anything
     // return object at end
 }
--- js/s4-problem-solving-approach/char-count.js
+++ js/s4-problem-solving-approach/char-count.js
@@ -1,5 +1,8 @@
 function charCount(str) {
     // make object
-    // loop over string
+    // loop over string, for each character
+        // if the char is a number / letter AND a key in object, add one to count
+        // if the char is a number / letter AND  not in object, add it and set value to 1
+        // if character is something else (space, period, etc. ) don't do anything
     // return object at end
 }
