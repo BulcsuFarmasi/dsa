f56dd629a8d23c8483c97c1512cd1832755dc79e
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-08-11 21:11:16 +0200

    for of js

--- js/s4-problem-solving-approach/char-count.js
+++ js/s4-problem-solving-approach/char-count.js
@@ -1,8 +1,8 @@
 function charCount(str) {
     // make object
     const result = {};
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i].toLowerCase();
+    for (let char of str) {
+        char = char.toLowerCase();
         if (/[a-z0-9]/.test(char))
         if (result[char] > 0) {
             result[char]++;
