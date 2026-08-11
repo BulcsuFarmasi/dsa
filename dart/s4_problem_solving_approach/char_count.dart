e24d55b9347bc66ecad15b356ee592115d1de603
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-08-11 21:05:43 +0200

    checking for alphanumeric chars

--- dart/s4_problem_solving_approach/char_count.dart
+++ dart/s4_problem_solving_approach/char_count.dart
@@ -1,24 +1,19 @@
 Map<String, int> charCount(final String str) {
-    // make object
-    final Map<String, int> result = {};
-    // loop over string, for each character
-    for (int i = 0; i < str.length; i++) {
-      final String char = str[i].toLowerCase();
-      // if the char is a number / letter AND a key in object, add one to count
+  final Map<String, int> result = {};
+  for (int i = 0; i < str.length; i++) {
+    final String char = str[i].toLowerCase();
+    if (RegExp(r'[a-z0-9]').hasMatch(char)) {
       if (result[char] != null) {
         result[char] = result[char]! + 1;
-      // if the char is a number / letter AND  not in object, add it and set value to 1
       } else {
         result[char] = 1;
       }
     }
+  }
 
-
-        // if character is something else (space, period, etc. ) don't do anything
-    // return object at end
-    return result;
+  return result;
 }
 
 void main() {
-  print(charCount("hello"));
+  print(charCount("Hi there!"));
 }
--- js/s4-problem-solving-approach/char-count.js
+++ js/s4-problem-solving-approach/char-count.js
@@ -1,22 +1,16 @@
 function charCount(str) {
     // make object
     const result = {};
-    // loop over string, for each character
     for (let i = 0; i < str.length; i++) {
-
-       let char = str[i].toLowerCase();
-        // if the char is a number / letter AND a key in object, add one to count
+        let char = str[i].toLowerCase();
+        if (/[a-z0-9]/.test(char))
         if (result[char] > 0) {
             result[char]++;
-        
-        // if the char is a number / letter AND  not in object, add it and set value to 1
         } else {
             result[char] = 1;
         }
     }
 
-        // if character is something else (space, period, etc. ) don't do anything
-    // return object at end
     return result;
 }
 
