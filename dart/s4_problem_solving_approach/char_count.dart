990215c377c7af2b297f552c333b563400dfde23
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-08-11 21:48:54 +0200

    different solution for alphanumeric check

--- dart/s4_problem_solving_approach/char_count.dart
+++ dart/s4_problem_solving_approach/char_count.dart
@@ -1,17 +1,22 @@
 Map<String, int> charCount(final String str) {
   final Map<String, int> result = {};
   for (int i = 0; i < str.length; i++) {
-    final String char = str[i].toLowerCase();
-    if (RegExp(r'[a-z0-9]').hasMatch(char)) {
-      if (result[char] != null) {
-        result[char] = result[char]! + 1;
-      } else {
-        result[char] = 1;
-      }
+    String char = str[i].toLowerCase();
+    if (isAlphaNumeric(char)) {
+        result[char] = (result[char] ?? 0) + 1;
     }
   }
 
   return result;
+}
+
+bool isAlphaNumeric(final String char) {
+  final int code = char.codeUnitAt(0);
+      if (!(code > 47 && code < 58) &&// numeric
+        !(code > 96 && code < 123)) { // lower alpha
+        return false;
+    }
+  return true;
 }
 
 void main() {
--- js/s4-problem-solving-approach/char-count.js
+++ js/s4-problem-solving-approach/char-count.js
@@ -2,16 +2,24 @@ function charCount(str) {
     // make object
     const result = {};
     for (let char of str) {
-        char = char.toLowerCase();
-        if (/[a-z0-9]/.test(char))
-        if (result[char] > 0) {
-            result[char]++;
-        } else {
-            result[char] = 1;
+
+        if (isAlphaNumeric(char)) {
+            char = char.toLowerCase();
+            result[char] = ++result[char] || 1;
         }
     }
 
     return result;
+}
+
+function isAlphaNumeric(char) {
+    const code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) && // numeric
+        !(code > 96 && code < 123)) { // lower alpha
+        return false;
+    }
+    return true;
 }
 
 console.log(charCount("Hi there!"));
