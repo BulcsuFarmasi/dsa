46e6f89c1d19506f80de16f541ed10c82cd1067b
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-08-19 16:20:23 +0200

    same nested

---
+++ dart/s5-problem-solving-approaches/same_nested.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+bool same(List<int> list1, List<int> list2) {
+  if (list1.length != list2.length) {
+    return false;
+  }
+
+  for (int i = 0; i < list1.length; i++) {
+    int? foundIndex;
+    for (int j = 0; j < list2.length; j++) {
+      if (pow(list1[i], 2) == list2[j]) {
+        foundIndex = j;
+        break;
+      }
+    }
+    if (foundIndex != null) {
+      list2.removeAt(foundIndex);
+    } else {
+      return false;
+    }
+  }
+  return list2.isEmpty;
+}
+
+void main(List<String> args) {
+  print(same([4,4,2], [16, 4]));
+}
